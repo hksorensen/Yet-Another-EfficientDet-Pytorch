d15ce42c6699d85d45774df827c6cb279dceecb7
Henrik Kragh Sørensen <hkragh@henrikkragh.dk>
2022-10-22 23:16:37 +0200

    Simple

--- src/dh4pmp_efficientdet/utils/utils.py
+++ src/dh4pmp_efficientdet/utils/utils.py
@@ -67,7 +67,7 @@ def aspectaware_resize_padding(image, width, height, interpolation=None, means=N
 
 def preprocess_from_path(*image_path, max_size=512, mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)):
     ori_imgs = [cv2.imread(img_path) for img_path in image_path]
-    return preprocess_from_imgs(ori_imgs, max_size=512, mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225))
+    return preprocess_from_imgs(ori_imgs, max_size=max_size, mean=mean, std=std)
 
 def preprocess_from_imgs(ori_imgs, max_size=512, mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)):
     normalized_imgs = [(img[..., ::-1] / 255 - mean) / std for img in ori_imgs]
